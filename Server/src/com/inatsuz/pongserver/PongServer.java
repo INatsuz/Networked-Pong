a81b9e0d3e79fc467cc86ab56982911f257d85a6
INatsuz <vascoraminhos@hotmail.com>
2016-09-18 02:21:50 +0100

    Finished
    Finished the game and added a console to the server

--- Server/src/com/inatsuz/pongserver/PongServer.java
+++ Server/src/com/inatsuz/pongserver/PongServer.java
@@ -1,43 +1,92 @@
 package com.inatsuz.pongserver;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.SocketException;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
 import javax.swing.Timer;
 
-public class PongServer implements ActionListener {
+public class PongServer implements ActionListener, KeyListener {
 
     protected static boolean running = false;
 
     private int MAX_CLIENTS = 2;
     private Clients[] clients = new Clients[MAX_CLIENTS];
 
-    protected static final int PORT = 2509;
+    protected static int PORT = 2509;
+    private String ip;
     private DatagramSocket socket;
 
     private final int BALL_SPEED = 12;
     private final int WIDTH = 700, HEIGHT = 700;
     private Ball ball;
 
+    private JFrame jFrame;
+    private JTextPane textPane;
+    private JTextPane commandPane;
+    private JScrollPane scrollPane;
+
     private Thread receive, send;
 
     private static PongServer pongServer;
 
     public PongServer() {
+        textPane = new JTextPane();
+        commandPane = new JTextPane();
+        scrollPane = new JScrollPane(textPane);
+        textPane.setEditable(false);
+        textPane.setBackground(Color.GRAY.darker().darker().darker());
+        textPane.setForeground(Color.GREEN);
+        commandPane.setBackground(Color.GRAY.darker().darker().darker());
+        commandPane.setForeground(Color.WHITE);
+        commandPane.setCaretColor(Color.WHITE);
+        commandPane.setSize(650, 100);
+        commandPane.setLocation(18, 525);
+        scrollPane.setSize(650, 500);
+        scrollPane.setLocation(18, 10);
+        scrollPane.setBorder(BorderFactory.createLineBorder(Color.GRAY.darker().darker()));
         System.out.println("Server Starting");
+        textPane.setText(textPane.getText() + " Server Starting \n");
         running = true;
         try {
-            socket = new DatagramSocket(PORT);
+            ip = JOptionPane.showInputDialog("Insert IP (Leave empty for local IP)");
+            PORT = Integer.parseInt(JOptionPane.showInputDialog("Insert Port (If using local IP you will need to forward this port)"));
+            if (!ip.equalsIgnoreCase("")) {
+                socket = new DatagramSocket(null);
+                InetSocketAddress address = new InetSocketAddress(ip, PORT);
+                socket.bind(address);
+            } else {
+                socket = new DatagramSocket(PORT);
+            }
+            jFrame = new JFrame("Server");
+            jFrame.setSize(700, 700);
+            jFrame.getContentPane().setBackground(Color.GRAY.darker());
+            jFrame.setLayout(null);
+            jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            jFrame.setLocationRelativeTo(null);
+            jFrame.add(scrollPane);
+            jFrame.add(commandPane);
+            commandPane.addKeyListener(this);
+            jFrame.setVisible(true);
             receive();
         } catch (SocketException ex) {
             System.out.println("Server Failed To Start");
             ex.printStackTrace();
         }
         System.out.println("Server Started");
+        textPane.setText(textPane.getText() + " Server Started \n");
         ball = new Ball(WIDTH / 2 - 10, HEIGHT / 2 - 10, clients, pongServer);
         Timer timer = new Timer(20, this);
         timer.start();
@@ -74,6 +123,7 @@ public class PongServer implements ActionListener {
                     sendPacket("id/" + i, packet.getAddress(), packet.getPort());
                     clients[i].setX(700 * i - 50 * i);
                     System.out.println(i);
+                    textPane.setText(textPane.getText() + " User Joined \n");
                     return;
                 }
             }
@@ -182,6 +232,37 @@ public class PongServer implements ActionListener {
         for (int i = 0; i < MAX_CLIENTS; i++) {
             clients[i].score = 0;
         }
+    }
+
+    private void checkConsoleCommand(String command) {
+        if (command.equalsIgnoreCase("/stop")) {
+            textPane.setText(textPane.getText() + " " + "Server Stopping" + "\n");
+            System.exit(0);
+        } else if(command.equalsIgnoreCase("/ip")){
+            textPane.setText(textPane.getText() + " " + ip + "\n");
+        }else {
+            textPane.setText(textPane.getText() + " " + "That command does not exist" + "\n");
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            textPane.setForeground(Color.WHITE);
+            e.consume();
+            String command = commandPane.getText();
+            textPane.setText(textPane.getText() + " " + command + "\n");
+            commandPane.setText(null);
+            commandPane.setCaretPosition(0);
+            if (command.startsWith("/")) {
+                checkConsoleCommand(command);
+            }
+        }
+    }
+
+    public void keyTyped(KeyEvent e) {
+    }
+
+    public void keyReleased(KeyEvent e) {
     }
 
 }
